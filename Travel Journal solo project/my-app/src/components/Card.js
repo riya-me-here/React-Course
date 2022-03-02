9e2cbf3a033c23200c8abc77cdf9f97643cd3fc5
Riya Mehere <riyamehere@gmail.com>
2022-03-02 09:58:25 +0530

    made few changes in design

--- Travel Journal solo project/my-app/src/components/Card.js
+++ Travel Journal solo project/my-app/src/components/Card.js
@@ -8,12 +8,16 @@ export default function Card(props) {
       <div className="card-info">
         <img src={locationIcon} alt="location-img" className="location-icon" />
         <span className="country"> {props.country}</span>
+        <a href={props.url} className="url">
+          Click to view on map
+        </a>
         <br />
         <p className="place"> {props.place}</p>
         <br />
         <span className="date"> {props.date}</span>
         <br />
         <span className="desc"> {props.description}</span>
+        <hr className="hr-line" />
       </div>
     </div>
   );
--- Travel Journal solo project/my-app/src/data.js
+++ Travel Journal solo project/my-app/src/data.js
@@ -8,6 +8,7 @@ export default [
     description:
       "Fondly known as the ‘City of the Taj’, Agra is a must- visit city on any tour of India. Located 220 km from Delhi, the city provides avid glimpses of Mughal architecture. Formerly the capital of the Mughal Empire, the culture and heritage still exists in the bylanes of the city alluring tourists from far ends of the globe. The view of the Taj gives more than plenty reasons to pay a visit to Agra",
     openSpots: 0,
+    url: "https://goo.gl/maps/o7vs6PJa5hPMjbEQ9",
   },
   {
     id: 2,
@@ -18,6 +19,7 @@ export default [
     description:
       "Goa, state of India, comprising a mainland district on the country’s southwestern coast and an offshore island. It is located about 250 miles (400 km) south of Mumbai (Bombay). One of India’s smallest states, it is bounded by the states of Maharashtra on the north and Karnataka on the east and south and by the Arabian Sea on the west. ",
     openSpots: 0,
+    url: "https://goo.gl/maps/rqS7UFYd798MQcnJ6",
   },
   {
     id: 3,
@@ -28,6 +30,7 @@ export default [
     description:
       "Manali is the real starting point of an ancient trade route which crosses the Rohtang and Baralacha passes, and runs via Lahul and Ladakh to Kashmir while divergent road connects it with Spiti. Now the motor link have been provided up to Leh in Jammu & Kashmir, Pangi valley in Chamba and Kaza of Lahul & Spiti. There are regular bus services to these places from Manali during summer season. It is situated at a distance of 45 kms from Kullu.",
     openSpots: 0,
+    url: "https://goo.gl/maps/nv5gXmGVRTAjAVru5",
   },
   {
     id: 4,
@@ -38,5 +41,6 @@ export default [
     description:
       "Rajasthan, state of northwestern India, located in the northwestern part of the Indian subcontinent. It is bounded to the north and northeast by the states of Punjab and Haryana, to the east and southeast by the states of Uttar Pradesh and Madhya Pradesh, to the southwest by the state of Gujarat, and to the west and northwest by the provinces of Sindh and Punjab in Pakistan. The capital city is Jaipur, in the east-central part of the state.",
     openSpots: 0,
+    url: "https://goo.gl/maps/jsbffXbDnP89gsjZ8",
   },
 ];
